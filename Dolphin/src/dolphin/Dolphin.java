0b8c77abaf641b38ea717f88ccb94206c5c3d893
Erick <erick.gallegos.est@tecazuay.edu.ec>
2021-10-08 22:34:41 -0500

    borre prueba

--- Dolphin/src/dolphin/Dolphin.java
+++ Dolphin/src/dolphin/Dolphin.java
@@ -20,10 +20,7 @@ public class Dolphin {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Modelo_Disciplina modelo= new Modelo_Disciplina();
-        VistaDisciplina vista =new VistaDisciplina();
-        Control_Disciplina control= new Control_Disciplina(modelo, vista);
-        control.iniciaControl();
+       
     }
     
 }
