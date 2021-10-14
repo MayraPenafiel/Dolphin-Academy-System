dda7e507e95b767a2278be3e1f9788322582a150
25Pablo <pablo25sumba@outlook.com>
2021-10-13 20:44:26 -0500

    Update Dolphin.java

--- Dolphin/src/dolphin/Dolphin.java
+++ Dolphin/src/dolphin/Dolphin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dolphin;
 
 import Vista.VistaInicio;
@@ -13,14 +9,13 @@ import Vista.VistaInicio;
  *
  * @author User
  */
-
 public class Dolphin {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        VistaInicio VI = new VistaInicio();
+        
        
     }
     
