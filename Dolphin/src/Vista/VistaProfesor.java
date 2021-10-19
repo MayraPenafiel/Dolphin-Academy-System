ad1329365f429c84db64d0cba4f004ade808e69c
MayraPenafiel <mayra.penafiel.est@tecazuay.edu.ec>
2021-10-18 21:18:18 -0500

    Vistas para el Menu
    Cambie las vistas para que al momento de salir en el menu salgan dentro de este

--- Dolphin/src/Vista/VistaProfesor.java
+++ Dolphin/src/Vista/VistaProfesor.java
@@ -16,11 +16,13 @@ import javax.swing.JTextField;
  *
  * @author HP
  */
-public class VistaProfesor extends javax.swing.JFrame {
+
+public class VistaProfesor extends javax.swing.JInternalFrame {
 
     /**
      * Creates new form VistaProfesor
      */
+    
     public VistaProfesor() {
         initComponents();
     }
@@ -449,10 +451,7 @@ public class VistaProfesor extends javax.swing.JFrame {
     }//GEN-LAST:event_TxtCodProfActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        this.dispose();
-        VistaMenu vm = new VistaMenu();
-        vm.setVisible(true);
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     public JTextField getTxtFormacionProf() {
@@ -526,47 +525,6 @@ public class VistaProfesor extends javax.swing.JFrame {
     public void setTxtContraseñaProf(JTextField TxtContraseñaProf) {
         this.TxtContraseñaProf = TxtContraseñaProf;
     }
-    
-    
-
-//    /**
-//     * @param args the command line arguments
-//     */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(VistaProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(VistaProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(VistaProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(VistaProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new VistaProfesor().setVisible(true);
-//            }
-//        });
-//    }
-    
-    //GETTERS Y SETTER
-    
-    
     
     public JButton getBtnCrearJFProf() {
         return BtnCrearJFProf;
@@ -728,9 +686,6 @@ public class VistaProfesor extends javax.swing.JFrame {
         this.btnimprimirprofe = btnimprimirprofe;
     }
     
-    
-    
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnCrearJFProf;
     private javax.swing.JButton BtnCrearProf;
